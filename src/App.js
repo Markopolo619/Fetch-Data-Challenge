8b4bfd4a5dc265b20ccb4bca76354b3de6a646bb
Job-madi <job.madi@protonmail.com>
2021-11-07 18:42:01 +0200

    Styled components and import components

---
+++ src/App.js
@@ -0,0 +1,33 @@
+// import useState and useEffect from react
+import React, { useState, useEffect } from 'react';
+import Form from './Components/Form';
+import List from './Components/List';
+
+function App() {
+  const API_URL = 'https://jsonplaceholder.typicode.com/'
+  const [reqType, setReqType] = useState('users')
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    const fetchItems = async () => {
+      try {
+        const response = await fetch(`${API_URL}${reqType}`)
+        const data = await response.json()
+
+        setItems(data)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchItems()
+  }, [reqType])
+
+  return (
+    <div className="App">
+      <Form reqType={reqType} setReqType={setReqType} />
+      <List items={items} />
+    </div>
+  );
+}
+
+export default App;
